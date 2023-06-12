282c357260574e4145c6ac21c73e6862e8d1d337
Taichi Eto <38776373+TaichiEto@users.noreply.github.com>
2023-06-12 14:29:39 +0900

    第８回課題
    ６番のみ

---
+++ 1b-8/script.js
@@ -0,0 +1,11 @@
+function execute(){
+    for(let i = 1; i<100; i++){
+        document.write(i);
+        if((i%3 == 0)||(i >= 30 && i < 40 && i % 30 < 10)||(i % 10 == 3)){
+            document.write("アホ<br>");
+        }
+        else{
+            document.write("<br>");
+        }
+    }
+}
