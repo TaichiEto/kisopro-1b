18cb7cd8700f74915fcc68456f86d24426b3e094
Taichi Eto <38776373+TaichiEto@users.noreply.github.com>
2023-06-12 14:44:02 +0900

    Update script.js

--- 1b-8/script.js
+++ 1b-8/script.js
@@ -1,7 +1,7 @@
 function execute(){
     for(let i = 1; i<100; i++){
         document.write(i);
-        if((i%3 == 0)||(i >= 30 && i < 40 && i % 30 < 10)||(i % 10 == 3)){
+        if((i%3 == 0)||(i >= 30 && i < 40)||(i % 10 == 3)){
             document.write("アホ<br>");
         }
         else{
