d0a002781edd91412c6f664aa84223ca96f38a61
Taichi Eto <38776373+TaichiEto@users.noreply.github.com>
2023-06-16 18:03:06 +0900

    Update script.js

--- 1b-9/script.js
+++ 1b-9/script.js
@@ -1,3 +1,8 @@
+function debug(a){
+    alert(a + 1);
+}
+
+
 function tenSteps () {
     for (let i = 0; i <= 9; i ++){
         for (let j = i; j >= 0; j --){
@@ -27,22 +32,22 @@ function nStepsOppsite(){
     }
 }
 
-function isPrimeNumber(p){
-    l = Number(p);
-    let smallest_divisor = -100;
-    for (let guess = 0; guess <= l; guess++){
-        if(l % guess == 0 && l != guess && guess != 1 && l != 1){
-            // document.getElementById("result").innerHTML = "素数ではありません";
-            smallest_divisor = guess;
-        }
-    }
-    if (smallest_divisor == -100){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+// function isPrimeNumber(p){
+//     l = Number(p);
+//     let smallest_divisor = -100;
+//     for (let guess = 0; guess <= l; guess++){
+//         if(l % guess == 0 && l != guess && guess != 1 && l != 1){
+//             // document.getElementById("result").innerHTML = "素数ではありません";
+//             smallest_divisor = guess;
+//         }
+//     }
+//     if (smallest_divisor == -100){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
 
 function displayPrimeNumber(){
     let z = Number(document.getElementById("superValue").value);
@@ -56,9 +61,17 @@ function displayPrimeNumber(){
 
 function superValues(){
     let y = Number(document.getElementById("superValue").value);
-    for (let estimation = 1; estimation <= y; estimation++){
-        if (isPrimeNumber(estimation)){
-            document.write(estimation + "<br>");
+    for (let guess = 2; guess < y; guess++){
+        let flag = 0;
+        for (let estimation = 2; estimation < guess; estimation++){
+            if(guess % estimation == 0){
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0){
+            document.write(guess + "<br>");
         }
     }
 }
+
