d6dd1edb7dc2a6ed2ebcbac8f5c8b9fcaffb0dc5
Taichi Eto <38776373+TaichiEto@users.noreply.github.com>
2023-07-14 17:17:33 +0900

    otsuka ryohei

---
+++ 1b-13/script.js
@@ -0,0 +1,9 @@
+function test(){
+    let x = ("<img src=penguin.jpeg>")
+    document.getElementById("a").innerHTML = x;
+}
+
+function test2(){
+    let y = ("<img src=ningen.JPG>");
+    document.getElementById("a").innerHTML = y;
+}
